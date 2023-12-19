cf1ccb05651ab5e202331dc3c48c8e6b767d140d
Matteo Murante <m.murante@x-party.it>
2023-12-19 22:08:11 +0100

    removed ci-test

--- src/main.js
+++ src/main.js
@@ -6,16 +6,6 @@ const core = require('@actions/core')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
